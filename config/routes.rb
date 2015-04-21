55b9e974fb8b7a26f9180afd794d8c6117442a5f
akhsunna <akhsunna4@gmail.com>
2015-04-21 12:21:38 +0300

    add users list & change routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   resources :users
 
-  root to: 'users#show'
+  root to: 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
