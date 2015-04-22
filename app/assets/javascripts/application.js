3069a2e2e21a96b103bb7f46607d5e5133dc99dc
akhsunna <akhsunna4@gmail.com>
2015-04-22 12:55:55 +0300

    change design LogIn page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -32,3 +32,14 @@ function changeUserStauts()
         $("#user_group_id").val(1);
     }
 }
+
+function showPassword() {
+    var key_attr = $('#key').attr('type');
+    if(key_attr != 'text') {
+        $('.checkbox').addClass('show');
+        $('#key').attr('type', 'text');
+    } else {
+        $('.checkbox').removeClass('show');
+        $('#key').attr('type', 'password');
+    }
+}
