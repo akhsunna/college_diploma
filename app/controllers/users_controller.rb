321d0b6b680cdc947bb7b7f63286c04bc12864c4
akhsunna <akhsunna4@gmail.com>
2015-04-24 14:20:46 +0300

    add menu and divide users views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
+
+
+  def main
+    if user_signed_in?
+      render current_user.teacher? ? 'users/teacher/home' : 'users/student/home'
+    else
+      redirect_to new_user_session_path
+    end
+  end
+
   def show
     @user = User.find(params[:id])
   end
